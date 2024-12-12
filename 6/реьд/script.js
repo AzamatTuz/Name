0d7d982e6acf222e397a61b9b9d54c404bad1e51
AzamatTuz <tuzelbajazamat361@gmail.com>
2024-12-12 14:01:55 +0300

    Add files via upload

---
+++ 6/реьд/script.js
@@ -0,0 +1,9 @@
+let nameIs = prompt('Атынды жаз бас арыппен бастап' + ' пример: Azamat либо Азамат');
+
+if (nameIs == 'Елбек' || nameIs == 'Elbek') {
+    alert(`${nameIs} чёрт`)
+} else if (nameIs == 'Azamat' || nameIs == 'Азамат') {
+    alert(`${nameIs} лучший`)
+} else {
+    alert('Кым ол')
+}
